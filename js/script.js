d211cf8ab101eb089784cdbbcccefd60b2f66a27
monika_loza <monika.loza@gmail.com>
2018-03-07 09:52:33 +0100

    usunięcie komentarzy

--- js/script.js
+++ js/script.js
@@ -86,44 +86,4 @@ $(document).ready(function () {
         $(this).toggleClass("active");
     });
 
-
-    /* SCROLL NAV */
-
-    /*window.onscroll = function () { myFunction() };
-
-    var navbar = $(".main-nav .nav li");
-    var navHeight = $(".main-nav.fixed-top").height();
-    console.log(navHeight);
-
-    var sticky = navbar.offset().top;
-
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            $(".main-nav, #about").addClass("fixed-top")
-            $("#about").css("padding-top", navHeight);
-        } else {
-            $(".main-nav, #about").removeClass("fixed-top");
-            $("#about").css("padding-top", 0);
-        }
-    }*/
-
-    /*var scrollLogo = $(".logo").offset().top;
-    var heightHeader = $("header").height();
-    var mainHeight = $(".main-nav.fixed-top").height();
-    var sum = heightHeader + mainHeight;
-
-
-    $(document).scroll(function () {
-        var scrollTop = $(document).scrollTop();
-
-        $(".main-nav").toggleClass("fixed-top", scrollTop > scrollLogo);
-
-        if(scrollTop > scrollLogo){
-            $("#about").css("margin-top", heightHeader/2 );
-        } else {
-            $("#about").css("margin-top",heightHeade/2 );
-        }
-    });*/
-
-
 });
